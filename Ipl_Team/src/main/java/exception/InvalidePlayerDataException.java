a761260fff6308f554cd69e3d4835f1f8eb9f1df
aniketgonjare <130743811+aniketgonjare@users.noreply.github.com>
2023-06-13 07:37:13 +0530

    Delete Ipl_Team directory

--- Ipl_Team/src/main/java/exception/InvalidePlayerDataException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-@SuppressWarnings("serial")
-public class InvalidePlayerDataException extends Exception {
-	public InvalidePlayerDataException(String msg)
-	{
-		super(msg);
-	}
-}
